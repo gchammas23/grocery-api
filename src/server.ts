9cedc1f9181f930b4188fd6f1199d56f2e2ee8df
Gabriel Chammas <gabychammas23@gmail.com>
2025-07-22 18:57:45 +0300

    Add starter server

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
